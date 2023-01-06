4141c9290170d87d49be7edb5d677b8d8e66a7ff
034qwe <bnestercuk91@gmail.com>
2023-01-06 20:36:27 +0200

    views

--- webproject/main/views.py
+++ webproject/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request,'main/list.html')
+
+def me(request):
+    return render(request,'main/list2.html')
