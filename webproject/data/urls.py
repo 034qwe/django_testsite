35436d52c64fdac98515fd70bc1ed6e0069d68a2
034qwe <bnestercuk91@gmail.com>
2023-01-10 20:06:39 +0200

    database and start func add record in database

--- webproject/data/urls.py
+++ webproject/data/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.data,name='data'),
+    path('add',views.add,name='create'),
 ]
--- webproject/data/views.py
+++ webproject/data/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import Articles
 
 # Create your views here.
 
 def data(request):
-    return render(request,'data/data.html')
+    data_articles=Articles.objects.all()
+    return render(request,'data/data.html',{"data_art":data_articles})
+
+
+def add(request):
+    return render(request,'data/add.html')
