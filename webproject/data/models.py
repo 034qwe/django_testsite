969ce5a56f8e9b09e475dc9e7ae9d5745489809b
034qwe <bnestercuk91@gmail.com>
2023-01-27 20:54:03 +0200

    new del database

--- webproject/data/models.py
+++ webproject/data/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+
+
 class Articles(models.Model):
     title=models.CharField('title',max_length=40)
     anons =models.CharField('beginning',max_length=250)
     main_text=models.TextField('article')
-    date = models.DateField('date of publication')
+    date = models.DateField('date of publication',auto_created=True)
+    categ = models.ForeignKey('Category_Articles',on_delete=models.PROTECT,null=True) #+= _id
 
     def get_absolute_url(self):
-        return f'/news/{self.id}'
+        return reverse('post', kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
+
+
+class Category_Articles(models.Model):
+    name = models.CharField(max_length=150,db_index=True)
+    slug = models.SlugField(max_length=255,unique=True,db_index=True, verbose_name="URL")
+
+    def __str__(self):
+        return self.name
 
 
 class Scientists(models.Model):
@@ -41,4 +52,4 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id':self.pk})
 
-    
+
