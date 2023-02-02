1696a9d5d20b3f32fe14fe894650549232caa4dc
034qwe <bnestercuk91@gmail.com>
2023-02-02 20:45:28 +0200

    new form

--- webproject/data/forms.py
+++ webproject/data/forms.py
@@ -1,4 +1,5 @@
-from .models import Articles
+from .models import *
+from django import forms
 from django.forms import ModelForm,TextInput,DateInput,Textarea,ImageField
 
 
@@ -25,4 +26,14 @@ class ArticlesForm(ModelForm):
                 'placeholder': 'date',
             }),
 
-        } 
+        }
+    
+
+
+class AddPost(forms.Form):
+    title = forms.CharField(max_length=40)
+    slug = forms.SlugField(max_length=255,)
+    anons =forms.CharField(max_length=250)
+    main_text=forms.Textarea()
+    categ = forms.ModelChoiceField(queryset=Category_Articles.objects.all())
+    is_published = forms.BooleanField() 
