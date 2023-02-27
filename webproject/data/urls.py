39a6caa27693383bfff990c4115bf7c7a5ccb717
034qwe <bnestercuk91@gmail.com>
2023-02-27 20:37:17 +0200

    contact

--- webproject/data/urls.py
+++ webproject/data/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-    path('',views.DataPage.as_view(),name='data'),
+    path('',cache_page(60)(views.DataPage.as_view()),name='data'),
     path('add',views.AddPage.as_view(),name='create'),
     path('post/<slug:post_slug>',views.ShowPost.as_view(), name='news-detail'), 
     path('<int:pk>/update',views.DataUpdatelView.as_view(), name='news-update'),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('login/',views.LoginUser.as_view(),name='login'),
     path('category/<slug:cat_slug>/',views.show_category, name='category'),
     path('logout/',views.logout_user,name='logout'),
+    path('contact/',views.ContactView.as_view(),name='contact')
 
 ]
